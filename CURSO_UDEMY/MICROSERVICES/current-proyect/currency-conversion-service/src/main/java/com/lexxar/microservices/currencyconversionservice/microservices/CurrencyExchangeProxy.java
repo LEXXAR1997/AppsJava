bd1e8d44ba0f4ce1b8fc7fa0c8dbdbe3641f7d91
LEXXAR1997 <jiubermq97@gmail.com>
2021-08-30 18:24:31 -0500

    OpenFeign

---
+++ CURSO_UDEMY/MICROSERVICES/current-proyect/currency-conversion-service/src/main/java/com/lexxar/microservices/currencyconversionservice/microservices/CurrencyExchangeProxy.java
@@ -0,0 +1,17 @@
+package com.lexxar.microservices.currencyconversionservice.microservices;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.lexxar.microservices.currencyconversionservice.entities.CurrencyConversion;
+
+@FeignClient(name="currency-exchange", url="localhost:8000")
+public interface CurrencyExchangeProxy {
+	
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public CurrencyConversion retrieveExchangeValue(
+			@PathVariable("from") String from,
+			@PathVariable("to") String to);
+
+}
