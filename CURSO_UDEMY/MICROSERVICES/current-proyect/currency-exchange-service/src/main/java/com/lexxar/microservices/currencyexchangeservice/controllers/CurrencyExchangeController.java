cdf6b8c97cb8bf545bbe2ad5cc26ad31ea810058
LEXXAR1997 <jiubermq97@gmail.com>
2021-08-30 17:00:58 -0500

    Current conversion

--- CURSO_UDEMY/MICROSERVICES/current-proyect/currency-exchange-service/src/main/java/com/lexxar/microservices/currencyexchangeservice/controllers/CurrencyExchangeController.java
+++ CURSO_UDEMY/MICROSERVICES/current-proyect/currency-exchange-service/src/main/java/com/lexxar/microservices/currencyexchangeservice/controllers/CurrencyExchangeController.java
@@ -1,27 +1,40 @@
 package com.lexxar.microservices.currencyexchangeservice.controllers;
 
 import com.lexxar.microservices.currencyexchangeservice.Entities.CurrencyExchange;
+import com.lexxar.microservices.currencyexchangeservice.repositories.CurrencyExchangeRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.math.BigDecimal;
 
 @RestController
 public class CurrencyExchangeController {
 
+	@Autowired
+	private CurrencyExchangeRepository repository;
+	
     @Autowired
     private Environment environment;
 
-    @GetMapping(path = "/currency-exchange/{from}/USD/{to}/INR")
+    @GetMapping(path = "/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange RetrieveExchangeValue(
             @PathVariable String from, @PathVariable String to){
-        CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
-        String port = environment.getProperty("local.server.port");
-        currencyExchange.setEnvironment(port);
-        return currencyExchange;
+        
+    	CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
+
+		if(currencyExchange ==null) {
+			throw new RuntimeException
+				("Unable to Find data for " + from + " to " + to);
+		}
+		
+		String port = environment.getProperty("local.server.port");
+		
+		currencyExchange.setEnvironment(port);
+		
+		return currencyExchange;
     }
 
 }
---
+++ CURSO_UDEMY/MICROSERVICES/current-proyect/currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO currency_exchange VALUES (10001, 65, '', 'USD', 'INR'),
+									(10002, 75, '', 'EUR', 'INR'),
+									(10003, 25, '', 'AUD', 'INR'),
+									(10004, 20, '', 'USD', 'MXN');
